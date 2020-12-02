bd792ba58351004f03de230d2b74735e086fd679
sakkamade <62730422+sakkamade@users.noreply.github.com>
2020-12-02 18:12:22 +0300

    Update thingifier.user.js

--- thingifier.user.js
+++ thingifier.user.js
@@ -2,7 +2,7 @@
 // @name        Dynasty Thingifier
 // @namespace   Alice Cheshire
 // @author		Alice Cheshire
-// @downloadURL	https://github.com/roshiajin-kunai/Dynasty-Thingifier/raw/master/thingifier.user.js
+// @downloadURL	https://github.com/sakkamade/Dynasty-Thingifier/raw/master/thingifier.user.js
 // @include     https://dynasty-scans.com/*
 // @exclude     https://dynasty-scans.com/system/*
 // @exclude     https://dynasty-scans.com/*.json
